554635d0f2866cbe6d63f851d0c428d8fad6c20a
PE-611 <PE-611@yandex.ru>
2020-10-09 10:22:59 +0300

    The modules, module instances, the transfer of different parameters

--- Test.v
+++ Test.v
@@ -2,88 +2,41 @@ module Test (clk, a,b,c,d,e,f,g);
 input clk;
 output a,b,c,d,e,f,g;
 
+blink #(.q(50'd50000000)) bl1(.clk1(clk), .i(a));
+blink #(.q(50'd25000000)) bl2(.clk1(clk), .i(b));
 
 
-blink1 (.clk1(clk), .i(a));
-blink2 (.clk2(clk), .j(b));
 endmodule
 
 ////////////////////////////////////////
 
-module blink1 (clk1,i);
+module blink #(parameter q = 50'd50000000)(clk1,i);
 input clk1;
 output reg i;
 
 initial i <= 1'b0;
 
-reg[24:0] counter1;
-initial counter1 <= 25'd0;
+reg[49:0] counter;
+initial counter <= 50'd0;
 
 
 always @(posedge clk1) begin
-	counter1 <= counter1 + 1'b1;
-	if (counter1 == 25'd25000000) begin
-	counter1 <= counter1 - 25'd25000000;
+	counter <= counter + 1'b1;
+	if (counter == q) begin
+	counter <= counter - q;
 	i <= ~i;
 	end
 end
 endmodule
 
-////////////////////////////////////////
+//////////////////////////////////////////////////////
+//blink2 (.clk2(clk), .j(b)); Соединение между модулями, модули разные
+//blink3 (.clk3(clk), .k(c));
 
-module blink2 (clk2,j);
-input clk2;
-output reg j;
-
-initial j <= 1'b1;
-
-reg[24:0] counter2;
-initial counter2 <= 25'd0;
-
-
-always @(posedge clk2) begin
-	counter2 <= counter2 + 1'b1;
-	if (counter2 == 25'd8000000) begin
-	counter2 <= counter2 - 25'd25000000;
-	j <= ~j;
-	end
-end
-endmodule
+//blink1 bl1(.clk1(clk), .i(a)); Соединение между модулями, модули идентичны, создание экземпляров
+//blink1 bl2(.clk1(clk), .i(b));
 
 
 
-
-/*
-module Test 
-(
-input CLK, 
-output DS_C, 
-output DS_EN1, DS_EN2, DS_EN3, DS_EN4
-);
-
-assign {DS_EN1, DS_EN2, DS_EN3, DS_EN4} = 4'b1111;
-
-blink_gen (.clk(CLK));
-blink_gen (.clk2(DS_C));
-
-endmodule
-
-
-module blink_gen 
-(
-input clk, 
-output clk2
-);
-
-
-reg [25:0]cnt = 0;
-
-assign clk2 = cnt[25];
-
-always @(posedge clk) begin
-	cnt <= cnt + 26'b1;
-end
-
-
-endmodule
-*/
+//blink #(.q(50'd50000000)) bl1(.clk1(clk), .i(a)); Соединение между модулями, модули идентичны, создание экземпляров, передача параметра, у идентичных модулей разные параметры.
+//blink #(.q(25'd50000000)) bl2(.clk1(clk), .i(a));
