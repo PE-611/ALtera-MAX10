6625f1a2c2c669fc35f1d32d0fc7e52489c45de8
PE-611 <PE-611@yandex.ru>
2020-10-20 09:28:16 +0300

    create tips for creating instances, linking them to top module, and passing parameters

--- Test.v
+++ Test.v
@@ -1,17 +1,30 @@
+module Test();
+endmodule
+
+/*
 module Test (clk, a,b,c,d,e,f,g);
 input clk;
 output a,b,c,d,e,f,g;
 
-blink #(.q(50'd50000000)) bl1(.clk1(clk), .i(a));
-blink #(.q(50'd25000000)) bl2(.clk1(clk), .i(b));
+wire clk;
+
+
+PLL PLL_inst (               
+	.inclk0 (clk),
+	.c0 (clk1),
+	);
+
+
+blink #(.q(50'd50000000)) bl1(.clk2(clk1), .i(a));
+blink #(.q(50'd25000000)) bl2(.clk2(clk1), .i(b));
 
 
 endmodule
 
 ////////////////////////////////////////
 
-module blink #(parameter q = 50'd50000000)(clk1,i);
-input clk1;
+module blink #(parameter q = 50'd50000000)(clk2,i);
+input clk2;
 output reg i;
 
 initial i <= 1'b0;
@@ -20,7 +33,7 @@ reg[49:0] counter;
 initial counter <= 50'd0;
 
 
-always @(posedge clk1) begin
+always @(posedge clk2) begin
 	counter <= counter + 1'b1;
 	if (counter == q) begin
 	counter <= counter - q;
@@ -40,3 +53,4 @@ endmodule
 
 //blink #(.q(50'd50000000)) bl1(.clk1(clk), .i(a)); Соединение между модулями, модули идентичны, создание экземпляров, передача параметра, у идентичных модулей разные параметры.
 //blink #(.q(25'd50000000)) bl2(.clk1(clk), .i(a));
+*/
